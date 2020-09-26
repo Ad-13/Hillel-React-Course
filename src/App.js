bdb4b5dc0091c1913e92f0411875f40298374745
Andrew Krupin <ad.kru13@gmail.com>
2020-09-26 21:30:28 +0300

    Dz4 created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
-import Dz1 from './Dz1/Dz1';
+// import Dz1 from './Dz1/Dz1';
 // import Dz2 from './Dz2/Dz2';
 // import Dz3 from './Dz3/Dz3';
+import Dz4 from './Dz4/Dz4';
 
 function App() {
   return (
-    <Dz1 />
+    // <Dz1 />
     // <Dz2 />
     // <Dz3 />
+    <Dz4 />
   );
 }
 
---
+++ src/Dz4/Dz4.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import './Dz4.css';
+import Header from './modules/common/header/Header';
+import Footer from './modules/common/footer/Footer';
+import { useLocalStorage } from './shared/hooks/useLocalStorage';
+import { useOnlineStatus } from './shared/hooks/useOnlineStatus';
+import { useDocumentTitle } from './shared/hooks/useDocumentTitle';
+
+export default function Dz4() {
+  const [storedTheme, setTheme] = useLocalStorage('theme', 'white');
+
+  useEffect(() => {
+    console.log('useEffect');
+    setTheme('dark');
+  }, []);
+
+  console.log(useLocalStorage('theme'));
+  console.log(storedTheme);
+
+  // =================================================================
+
+  const online = useOnlineStatus();
+  console.log(`user is online: ${online}`);
+
+  // =================================================================
+
+  useDocumentTitle('Занятия | Hillel LMS');
+
+  return (
+    <div className='app'>
+      <Header />
+      <div className='app-content'>
+      </div>
+      <Footer />
+    </div>
+  )
+}
