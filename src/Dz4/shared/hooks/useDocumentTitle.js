c7015762cbb36498e79ab82ccdfbf545bbdb16cc
Andrew Krupin <ad.kru13@gmail.com>
2020-10-02 21:11:17 +0300

    DZ 4: hooks corrected

--- src/Dz4/shared/hooks/useDocumentTitle.js
+++ src/Dz4/shared/hooks/useDocumentTitle.js
@@ -1,3 +1,7 @@
+import { useEffect } from 'react';
+
 export function useDocumentTitle(title) {
-  document.title = title;
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
 }
--- src/Dz4/shared/hooks/useOnlineStatus.js
+++ src/Dz4/shared/hooks/useOnlineStatus.js
@@ -1,3 +1,22 @@
+import { useState, useEffect } from 'react';
+
 export function useOnlineStatus() {
-  return navigator.onLine;
+  const [isOnline, setIsOnline] = useState(window.navigator.onLine);
+
+  const updateOnlineStatus = () => {
+    setIsOnline(window.navigator.onLine);
+    console.log(window.navigator.onLine);
+  };
+  
+  useEffect(() => {
+    window.addEventListener("offline", updateOnlineStatus);
+    window.addEventListener("online", updateOnlineStatus);
+
+    return () => {
+        window.removeEventListener("offline", updateOnlineStatus);
+        window.removeEventListener("online", updateOnlineStatus);
+    };
+  });
+
+  return isOnline;
 }
