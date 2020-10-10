cd58fdca5ac67f8b483f8e1e8f9d7f9473774e2c
Andrew Krupin <ad.kru13@gmail.com>
2020-10-10 22:51:45 +0300

    dz5: photos slider implemented

--- src/Dz5/modules/AlbumPhotos/AlbumPhotos.js
+++ src/Dz5/modules/AlbumPhotos/AlbumPhotos.js
@@ -1,11 +1,22 @@
-import React, { useParams } from 'react';
+import React from 'react';
+import { useParams } from 'react-router-dom';
 import { useAlbumsPhotos } from '../../shared/hooks/useAlbumsPhotos';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Slider from "react-slick";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './AlbumPhotos.css';
 
 export default function AlbumPhotos() {
   const { albumId } = useParams();
   const [albumsPhotos, isFetching] = useAlbumsPhotos(albumId);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
 
   if (isFetching) {
     return (
@@ -18,6 +29,9 @@ export default function AlbumPhotos() {
   return (
     <>
       <div className="title">Album Photos</div>
+      <Slider className="slider" {...settings}>
+        {albumsPhotos.map(x => <img className="img" src={x.url} key={x.id} alt=""/>)}
+      </Slider>
     </>
   )
 }
